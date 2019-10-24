72a3270e320ea7cae9ea653c57b274de2bd882ff
XingmingQu <quxingming@qq.com>
2019-10-24 10:42:35 -0500

    add figure

---
+++ notebooksAndModules/modules/.ipynb_checkpoints/labelHelper-checkpoint.py
@@ -0,0 +1,97 @@
+
+# coding: utf-8
+
+# In[31]:
+
+print("Import an awesome Label helper")
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# In[32]:
+
+class Label:
+    def __init__(self, label_csv_file):
+        self.label_csv_file = pd.read_csv(label_csv_file)        
+        self.r_max,self.r_min=self._set_rmax_and_rmin()
+        self.trainee_dict=self._set_trainee_dict()
+
+    def _set_rmax_and_rmin(self):  
+        r_group=self.label_csv_file.groupby(['VideoNum'])
+        r_max=r_group.max()
+        r_min=r_group.min()
+        r_max=r_max.reset_index()
+        r_min=r_min.reset_index()
+        return r_max,r_min
+    
+    def _set_trainee_dict(self):
+        name_group=self.label_csv_file.groupby(['lastName'])
+        return dict(name_group.VideoNum.unique())
+        
+    def video_count(self):
+        counts=self.label_csv_file['VideoNum'].value_counts()
+        return len(counts)
+    
+    def get_review_times_by_video_number(self,videoNum):
+        counts=self.label_csv_file['VideoNum'].value_counts()
+        times=int(counts[counts.index==videoNum])
+        return times
+    
+    def get_video_score_range_by_video_number(self,videoNum):
+
+        vmax=self.r_max[self.r_max['VideoNum']==videoNum]
+        vmin=self.r_min[self.r_min['VideoNum']==videoNum]
+        return vmax,vmin
+    
+    def get_video_mean_label_by_video_number(self,videoNum):
+        group=self.label_csv_file[self.label_csv_file['VideoNum'] == videoNum]
+#         print(group)
+        return np.array(group.mean()[1:])
+
+    def get_video_median_label_by_video_number(self,videoNum):
+        group=self.label_csv_file[self.label_csv_file['VideoNum'] == videoNum]
+#         print(group)
+        return np.array(group.median()[1:])
+
+    def get_video_number(self):
+        return self.label_csv_file.VideoNum.unique()
+    
+    def get_trainee_info(self):
+        names=self.label_csv_file.lastName.unique()
+        print("There are %d trainees in total"%len(names))      
+        print("\n Number of Videos from each person")
+        peoples=list(self.trainee_dict.keys())
+        for people in peoples:
+        	video_number_list=self.get_video_number_by_name(people)
+        	print('%-15s' % people,"--",video_number_list,"----:",len(video_number_list))
+        
+    def get_video_number_by_name(self,name):
+        return self.trainee_dict[name]
+        
+    def train_test_split_on_people(self,test_ratio):
+        peoples=list(self.trainee_dict.keys())
+        train_people,test_people= train_test_split(peoples,test_size=test_ratio)
+        
+        print("train on videos from:\n",train_people)
+        print("test on videos from:\n",test_people)
+        
+        train_video_number=np.concatenate([self.get_video_number_by_name(people) for people in train_people], axis=None)
+        test_video_number=np.concatenate([self.get_video_number_by_name(people) for people in test_people], axis=None)
+        return train_video_number,test_video_number
+
+if __name__ == "__main__": 
+    print("test...")       
+
+# In[33]:
+
+
+# ll=Label('..//2019_fall_labels.csv')
+# N=ll.get_video_number()
+
+
+# In[20]:
+
+
+# a,b=ll.get_video_Scorerange(11)
+# ll.get_video_median_label(11)
+
---
+++ notebooksAndModules/modules/plotHelper.py
@@ -0,0 +1,74 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+from scipy.ndimage import maximum_filter1d
+from matplotlib.patches import Rectangle
+
+
+def plot(re,gt,vmax,vmin):
+    
+    Plot_data=pd.DataFrame(re,columns=['DP','BD','E','FS','A','RC'])
+    #max filter data
+    filtered_data=Plot_data.copy()
+    for index, row in filtered_data.iteritems():
+        filtered_data[index]=maximum_filter1d(filtered_data[index],mode='nearest',size= 3)
+        
+    def plot_predict_result(df):
+        y=[x for x in range(1,7)]
+        ax.plot(y, gt, label='ground truth',color='green')
+        ax.plot(y, np.mean(df,axis=0), label='mean predict',color='blue')
+#         ax.plot(y, np.median(df,axis=0), label='median predict',color='red')
+        ax.plot(y, np.round(np.mean(df,axis=0)), label='mean round predict',color='black')
+        plt.xticks(y)
+        plt.yticks(y_range)
+        labels=['DP','BD','E','FS','A','RC']
+        ax.set_xticklabels(labels)
+        ax.legend()    
+        it=1
+        for types in ['DP','BD','E','FS','A','RC']:
+            Range=vmax[types]-vmin[types]
+            rect = Rectangle((it, vmin[types]), 0.2, Range, color='green')
+            ax.add_patch(rect)   
+            it=it+1
+    
+    x_range = range(0,len(Plot_data),1)
+    y_range = np.linspace(1,5,num=9)    
+        
+    # raw data    
+    ax=plt.figure(figsize=(20,20))
+    ax = plt.subplot(421)
+    Plot_data.plot(ax=ax)
+    ax.legend(['DP','BD','E','FS','A','RC'],bbox_to_anchor=(1,1))
+    plt.ylabel('Gear Score')
+    plt.xlabel('Video clips length')
+    
+
+    plt.xticks(x_range)
+    plt.yticks(y_range)
+
+    # filtered data   
+    ax = plt.subplot(422)
+    filtered_data.plot(ax=ax)
+    ax.legend(['DP','BD','E','FS','A','RC'],bbox_to_anchor=(1,1))
+    plt.ylabel('Gear Score')
+    plt.xlabel('Video clips length')
+    plt.xticks(x_range)
+    plt.yticks(y_range)
+    
+    
+    ax = plt.subplot(423)
+    plot_predict_result(Plot_data)
+
+    ax = plt.subplot(424)
+    plot_predict_result(filtered_data)
+    
+    
+#     print("Ground Truth",gt)
+#     print("mean prediction MAE:", sum(abs(np.mean(Plot_data,axis=0)-gt)))
+#     print("median prediction MAE:", sum(abs(np.median(Plot_data,axis=0)-gt)))
+#     print("round prediction MAE:", sum(abs(np.round(np.mean(Plot_data,axis=0))-gt)))
+#     print('------------------------------------------------------------------------------')
+#     print("Filtered Ground Truth",gt)
+#     print("Filtered mean prediction MAE:", sum(abs(np.mean(filtered_data,axis=0)-gt)))
+#     print("Filteredmedian prediction MAE:", sum(abs(np.median(filtered_data,axis=0)-gt)))
+#     print("Filtered round prediction MAE:", sum(abs(np.round(np.mean(filtered_data,axis=0))-gt)))
